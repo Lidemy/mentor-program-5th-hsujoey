29a6050c101f4397ae7bfc345ba5cce2f32efa49
hsujoey <sesame0410@gmail.com>
2021-06-06 17:29:07 +0800

    week7

---
+++ homeworks/week7/hw3/hw3.js
@@ -0,0 +1,45 @@
+document.querySelector('.btn-add').addEventListener('click', () => {
+  const { value } = document.querySelector('.input-todo').value
+  if (!value) return
+  const div = document.createElement('div')
+  div.classList.add('todo')
+  div.innerHTML = `
+    <input class='todo-checkbox' type='checkbox'/>
+    <div class='todo-title'>${escapeHtml(value)}</div>
+    <button class="btn-del">🅇</button>
+    `
+  document.querySelector('.todos').appendChild(div)
+  document.querySelector('.input-todo').value = ''
+})
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+document.querySelector('.input-todo').addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault()
+    document.querySelector('.btn-add').click()
+  }
+})
+
+document.querySelector('.todos').addEventListener('click', (e) => {
+  const { target } = e
+  if (target.classList.contains('btn-del')) {
+    target.parentNode.remove()
+    return
+  }
+
+  if (target.classList.contains('todo-checkbox')) {
+    if (target.checked) {
+      target.parentNode.classList.add('done')
+    } else {
+      target.parentNode.classList.remove('done')
+    }
+  }
+})
